GNOME/gnome-boxes

commit 8ef80d81b0bad3754db65505504c3fcdc80911c5

Fix connect to libvirt machine with password

src/libvirt-machine.vala
@@ -26,8 +26,10 @@ private class Boxes.LibvirtMachine: Boxes.Machine {
 
     private ulong started_id;
     public override void connect_display () {
-        if (_connect_display == true)
+        if (_connect_display) {
+            update_display ();
             return;
+        }
 
         if (state != DomainState.RUNNING) {
             if (started_id != 0)
@@ -35,10 +37,10 @@ private class Boxes.LibvirtMachine: Boxes.Machine {
 
             if (state == DomainState.PAUSED) {
                 started_id = domain.resumed.connect (() => {
-                        domain.disconnect (started_id);
-                        started_id = 0;
-                        connect_display ();
-                    });
+                    domain.disconnect (started_id);
+                    started_id = 0;
+                    connect_display ();
+                });
                 try {
                     domain.resume ();
                 } catch (GLib.Error error) {
@@ -46,10 +48,10 @@ private class Boxes.LibvirtMachine: Boxes.Machine {
                 }
             } else {
                 started_id = domain.started.connect (() => {
-                        domain.disconnect (started_id);
-                        started_id = 0;
-                        connect_display ();
-                    });
+                    domain.disconnect (started_id);
+                    started_id = 0;
+                    connect_display ();
+                });
                 try {
                     domain.start (0);
                 } catch (GLib.Error error) {
@@ -246,6 +248,10 @@ private class Boxes.LibvirtMachine: Boxes.Machine {
 
     private void update_display () {
         string type, port, socket, host;
+
+        // TODO: this assertion holds true for the moment, I don't see
+        // yet why it shouldn't be
+        return_if_fail (_connect_display == true);
 
         try {
             var xmldoc = domain.get_config (0).to_xml();

commit 0f560dc52d618481d8df1d3238145a81906e962e

Fix disconnecting of display
I introduced a regression with 8ef80d81b0bad3754db65505504c3fcdc80911c5

src/libvirt-machine.vala
@@ -21,7 +21,7 @@ private class Boxes.LibvirtMachine: Boxes.Machine {
 
         _connect_display = false;
         app.display_page.remove_display ();
-        update_display ();
+        display = null;
     }
 
     private ulong started_id;
@@ -249,8 +249,6 @@ private class Boxes.LibvirtMachine: Boxes.Machine {
     private void update_display () {
         string type, port, socket, host;
 
-        // TODO: this assertion holds true for the moment, I don't see
-        // yet why it shouldn't be
         return_if_fail (_connect_display == true);
 
         try {