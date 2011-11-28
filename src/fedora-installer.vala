GNOME/gnome-boxes

commit a6f184c043bde504c6252f04406b7369b120e1ab

Drop redundant params of FedoraInstaller.extract_boot_files
https://bugzilla.gnome.org/show_bug.cgi?id=664766

src/fedora-installer.vala
@@ -17,15 +17,12 @@ private class Boxes.FedoraInstaller: UnattendedInstaller {
         if (!express_toggle.active)
             return;
 
-        var kernel_path = os_media.get_kernel_path ();
-        var initrd_path = os_media.get_initrd_path ();
-
-        if (kernel_path == null || initrd_path == null)
+        if (os_media.kernel_path == null || os_media.initrd_path == null)
             return;
 
         yield mount_media (cancellable);
 
-        yield extract_boot_files (kernel_path, initrd_path, cancellable);
+        yield extract_boot_files (cancellable);
 
         yield normal_clean_up (cancellable);
     }
@@ -81,11 +78,11 @@ private class Boxes.FedoraInstaller: UnattendedInstaller {
         mounted = true;
     }
 
-    private async void extract_boot_files (string kernel, string initrd, Cancellable? cancellable) throws GLib.Error {
-        kernel_path = Path.build_filename (mount_point, kernel);
-        kernel_file = File.new_for_path (kernel_path);
-        initrd_path = Path.build_filename (mount_point, initrd);
-        initrd_file = File.new_for_path (initrd_path);
+    private async void extract_boot_files (Cancellable? cancellable) throws GLib.Error {
+        kernel_path = Path.build_filename (mount_point, os_media.kernel_path);
+        kernel_file = File.new_for_path (os_media.kernel_path);
+        initrd_path = Path.build_filename (mount_point, os_media.initrd_path);
+        initrd_file = File.new_for_path (os_media.initrd_path);
 
         if (!mounted)
             return;

commit cbc7e441ee1a3d91465b8365ff07eb2109efb018

Fix regression introduced in a6f184c
Commit a6f184c totally broke the automated installation.

src/fedora-installer.vala
@@ -80,9 +80,9 @@ private class Boxes.FedoraInstaller: UnattendedInstaller {
 
     private async void extract_boot_files (Cancellable? cancellable) throws GLib.Error {
         kernel_path = Path.build_filename (mount_point, os_media.kernel_path);
-        kernel_file = File.new_for_path (os_media.kernel_path);
+        kernel_file = File.new_for_path (kernel_path);
         initrd_path = Path.build_filename (mount_point, os_media.initrd_path);
-        initrd_file = File.new_for_path (os_media.initrd_path);
+        initrd_file = File.new_for_path (initrd_path);
 
         if (!mounted)
             return;
@@ -97,7 +97,9 @@ private class Boxes.FedoraInstaller: UnattendedInstaller {
         var dest_file = File.new_for_path (dest_path);
 
         try {
+            debug ("Copying '%s' to '%s'..", file.get_path (), dest_path);
             yield file.copy_async (dest_file, 0, Priority.DEFAULT, cancellable);
+            debug ("Copied '%s' to '%s'.", file.get_path (), dest_path);
         } catch (IOError.EXISTS error) {}
 
         return dest_file;