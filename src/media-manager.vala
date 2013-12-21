GNOME/gnome-boxes

commit d231c2701b99c62563e861a5f4b436cde1697fb5

Add support for importing ready-made VMs
Currently only qcow2 and raw images are supported.

TODO:

* Report progress of import

https://bugzilla.gnome.org/show_bug.cgi?id=690757

src/media-manager.vala
@@ -25,20 +25,31 @@ private class Boxes.MediaManager : Object {
 
     public async InstallerMedia create_installer_media_for_path (string       path,
                                                                  Cancellable? cancellable = null) throws GLib.Error {
-        var media = yield new InstallerMedia.for_path (path, this, cancellable);
+        var media = is_mime_type (path, "application/x-cd-image") ?
+                    yield new InstallerMedia.for_path (path, this, cancellable) :
+                    new InstalledMedia (path);
 
         return create_installer_media_from_media (media);
     }
 
     public async InstallerMedia? create_installer_media_from_config (GVirConfig.Domain config) {
         var path = VMConfigurator.get_source_media_path (config);
+        if (path == null)
+            return null;
+
+        if (VMConfigurator.is_import_config (config))
+            try {
+                return new InstalledMedia (path);
+            } catch (GLib.Error error) {
+                debug ("%s", error.message);
+
+                return null;
+            }
+
         var label = config.title;
 
         Os? os = null;
         Media? os_media = null;
-
-        if (path == null)
-            return null;
 
         try {
             var os_id = VMConfigurator.get_os_id (config);

src/libvirt-broker.vala
@@ -56,6 +56,9 @@ private class Boxes.LibvirtBroker : Boxes.Broker {
             if (VMConfigurator.is_install_config (config) || VMConfigurator.is_live_config (config)) {
                 debug ("Continuing installation/live session for '%s', ..", machine.name);
                 new VMCreator.for_install_completion (machine); // This instance will take care of its own lifecycle
+            } else if (VMConfigurator.is_import_config (config)) {
+                debug ("Continuing import of '%s', ..", machine.name);
+                new VMImporter.for_import_completion (machine); // This instance will take care of its own lifecycle
             }
         } catch (GLib.Error error) {
             warning ("Failed to create source '%s': %s", source.name, error.message);

src/vm-configurator.vala
@@ -13,9 +13,11 @@ private class Boxes.VMConfigurator {
     private const string BOXES_XML = "<gnome-boxes>%s</gnome-boxes>";
     private const string LIVE_STATE = "live";
     private const string INSTALLATION_STATE = "installation";
+    private const string IMPORT_STATE = "importing";
     private const string INSTALLED_STATE = "installed";
     private const string LIVE_XML = "<os-state>" + LIVE_STATE + "</os-state>";
     private const string INSTALLATION_XML = "<os-state>" + INSTALLATION_STATE + "</os-state>";
+    private const string IMPORT_XML = "<os-state>" + IMPORT_STATE + "</os-state>";
     private const string INSTALLED_XML = "<os-state>" + INSTALLED_STATE + "</os-state>";
 
     private const string OS_ID_XML = "<os-id>%s</os-id>";
@@ -123,6 +125,10 @@ private class Boxes.VMConfigurator {
 
     public static bool is_live_config (Domain domain) {
         return get_os_state (domain) == LIVE_STATE;
+    }
+
+    public static bool is_import_config (Domain domain) {
+        return get_os_state (domain) == IMPORT_STATE;
     }
 
     public static StorageVol create_volume_config (string name, int64 storage) throws GLib.Error {
@@ -330,6 +336,8 @@ private class Boxes.VMConfigurator {
 
         if (installed)
             custom_xml = INSTALLED_XML;
+        else if (install_media is InstalledMedia)
+            custom_xml = IMPORT_XML;
         else
             custom_xml = (install_media.live) ? LIVE_XML : INSTALLATION_XML;
 

src/wizard-source.vala
@@ -309,6 +309,8 @@ private class Boxes.WizardSource: GLib.Object {
         dialog.local_only = true;
         dialog.filter = new Gtk.FileFilter ();
         dialog.filter.add_mime_type ("application/x-cd-image");
+        dialog.filter.add_pattern ("*.img");
+        dialog.filter.add_pattern ("*.qcow2");
         var ret = false;
         if (dialog.run () == Gtk.ResponseType.ACCEPT) {
             uri = dialog.get_uri ();

commit daaeec8358f0ae23a33ae1bb1f53c863a315d131

media-manager: More reliable check for media type
Instead of checking for mime-types on file to see if its an
installer/live media, we now rely on the check for installed media based
on file extension.

This fixes the regression from commit d231c27 which completely broke
handling of ISO9660 devices (loop-mounted ISOs and hardware).

https://bugzilla.gnome.org/show_bug.cgi?id=720801

src/media-manager.vala
@@ -27,10 +27,11 @@ private class Boxes.MediaManager : Object {
                                                                  Cancellable? cancellable = null) throws GLib.Error {
         InstallerMedia media;
 
-        if (is_mime_type (path, "application/x-cd-image"))
-            media = yield new InstallerMedia.for_path (path, this, cancellable);
-        else
+        try {
             media = yield new InstalledMedia.guess_os (path, this);
+        } catch (IOError.NOT_SUPPORTED e) {
+            media = yield new InstallerMedia.for_path (path, this, cancellable);
+        }
 
         return create_installer_media_from_media (media);
     }