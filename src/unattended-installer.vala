GNOME/gnome-boxes

commit d4bd651adb827f4a93ed6080048253620a6d1796

installer: Use the system cache for logos & drivers
https://bugzilla.gnome.org/show_bug.cgi?id=698144

src/unattended-installer.vala
@@ -498,10 +498,16 @@ private class Boxes.UnattendedInstaller: InstallerMedia {
             var file_uri = location + "/" + filename;
             var file = File.new_for_uri (file_uri);
 
-            var cached_path = get_drivers_cache (location_checksum + "-" + file.get_basename ());
+            var driver_file_name = location_checksum + "-" + file.get_basename ();
+            var cached_path = get_drivers_cache (driver_file_name);
+            var system_cached_path = get_system_drivers_cache (driver_file_name);
+
+            string[] cached_paths = { cached_path };
+            if (system_cached_path != null)
+                cached_paths += system_cached_path;
 
             var file_progress = progress.add_child_activity (file_progress_scale);
-            file = yield downloader.download (file, {cached_path}, file_progress);
+            file = yield downloader.download (file, cached_paths, file_progress);
             file_progress.progress = 1.0; // Ensure progress reaches 100%
 
             driver_files.append (new UnattendedRawFile (this, cached_path, filename));

src/downloader.vala
@@ -147,9 +147,15 @@ private class Boxes.Downloader : GLib.Object {
         debug ("%s has logo '%s'.", os.name, logo_url);
 
         var remote_file = File.new_for_uri (logo_url);
+        var system_cached_path = get_system_logo_cache (remote_file.get_basename ());
         var cached_path = get_logo_cache (remote_file.get_basename ());
+
+        string[] cached_paths = { cached_path };
+        if (system_cached_path != null)
+            cached_paths += system_cached_path;
+
         try {
-            var cached_file = yield get_instance ().download (remote_file, {cached_path});
+            var cached_file = yield get_instance ().download (remote_file, cached_paths);
             var pixbuf = new Gdk.Pixbuf.from_file_at_size (cached_file.get_path (), size, size);
             image.set_from_pixbuf (pixbuf);
         } catch (GLib.Error error) {

commit a6c6c2dcf9423dc8441d7f6f7395a6f97dd26b81

unattended-installer: Use correct cache path
d4bd651 caused a regression, where unattended installation setup failed
for OSes for which we download drivers (win7 and winxp) if the driver
files only exist in system cache since the code still tried to use the
user cache for all situations.

This patches fixes the issue by using the cache where there the file
exists/is downloaded.

src/unattended-installer.vala
@@ -513,7 +513,7 @@ private class Boxes.UnattendedInstaller: InstallerMedia {
             file = yield downloader.download (file, cached_paths, file_progress);
             file_progress.progress = 1.0; // Ensure progress reaches 100%
 
-            driver_files.append (new UnattendedRawFile (this, cached_path, filename));
+            driver_files.append (new UnattendedRawFile (this, file.get_path (), filename));
         }
 
         // We don't do this in above loop to ensure we have all the driver files