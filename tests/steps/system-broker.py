c9fc87d2b8d57a140126246016e3b07d735685c2
Zeeshan Ali (Khattak) <zeeshanak@gnome.org>
2015-02-03 14:22:00 +0000

    tests: Step to import multiple VMs to system broker

--- tests/steps/system-broker.py
+++ tests/steps/system-broker.py
@@ -10,11 +10,22 @@ def create_box_on_system_broker(context):
         * Wait for "sleep 10" end
         """)
 
+@step('Import Core-5.3 box on system broker as "{name}"')
+def import_box_on_system_broker(context, name):
+    if name != 'Core-5.3':
+        context.execute_steps("""
+                              * Wait for "cp /tmp/Core-5.3.qcow2 /tmp/%s.qcow2" end
+                              """ % name)
+
+    context.execute_steps("""
+        * Wait for "virt-install -q -r 128 --name %s --import --disk /tmp/%s.qcow2 --os-type linux --accelerate --connect qemu:///system --wait 0" end
+        * Wait for "sleep 10" end
+        """ % (name, name))
+
 @step('Import Core-5.3 box on system broker')
 def import_box_on_system_broker(context):
     context.execute_steps("""
-        * Wait for "virt-install -q -r 128 --name Core-5.3 --import --disk /tmp/Core-5.3.qcow2 --os-type linux --accelerate --connect qemu:///system --wait 0" end
-        * Wait for "sleep 10" end
+        * Import Core-5.3 box on system broker as "Core-5.3"
         """)
 
 @step('Connect to system broker')
