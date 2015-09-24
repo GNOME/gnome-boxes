GNOME/gnome-boxes

commit 03316ae271787a29c2b8e6a2a7eacc348280a50c

machine: is_on() now a property
This is needed to make how the machine's state is checked, consistent.

https://bugzilla.gnome.org/show_bug.cgi?id=752007

src/libvirt-machine.vala
@@ -645,7 +645,7 @@ private class Boxes.LibvirtMachine: Boxes.Machine {
     }
 
     public string? get_ip_address () {
-        if (system_virt_connection == null || !is_on ())
+        if (system_virt_connection == null || is_on)
             return null;
 
         var mac = get_mac_address ();

src/libvirt-machine-properties.vala
@@ -106,7 +106,7 @@ private class Boxes.LibvirtMachineProperties: GLib.Object, Boxes.IPropertiesProv
         var list = new List<Boxes.Property> ();
 
         // the wizard may want to modify display properties, before connect_display()
-        if (machine.is_on () && machine.display == null)
+        if (machine.is_on && machine.display == null)
             try {
                 machine.update_display ();
             } catch (GLib.Error e) {
@@ -479,7 +479,7 @@ private class Boxes.LibvirtMachineProperties: GLib.Object, Boxes.IPropertiesProv
                 property.changed.connect (on_ram_changed);
 
             machine.notify["state"].connect (() => {
-                if (!machine.is_on ())
+                if (!machine.is_on)
                     property.reboot_required = false;
             });
 

src/machine.vala
@@ -40,6 +40,14 @@ private abstract class Boxes.Machine: Boxes.CollectionItem, Boxes.IPropertiesPro
         }
     }
 
+    public bool is_on {
+        get {
+            return state == MachineState.RUNNING ||
+                state == MachineState.PAUSED ||
+                state == MachineState.SLEEPING;
+        }
+    }
+
     private ulong show_id;
     private ulong hide_id;
     private ulong disconnected_id;
@@ -383,12 +391,6 @@ private abstract class Boxes.Machine: Boxes.CollectionItem, Boxes.IPropertiesPro
             info = _("Saving…");
         else
             info = null;
-    }
-
-    public bool is_on () {
-        return state == MachineState.RUNNING ||
-            state == MachineState.PAUSED ||
-            state == MachineState.SLEEPING;
     }
 
     private void save_pixbuf_as_screenshot (Gdk.Pixbuf? pixbuf) {

src/properties.vala
@@ -77,7 +77,7 @@ private class Boxes.Properties: Gtk.Notebook, Boxes.UI {
             }
 
             var machine = window.current_item as Machine;
-            if (reboot_required && (machine.is_on () || machine.state == Machine.MachineState.SAVED)) {
+            if (reboot_required && (machine.is_on || machine.state == Machine.MachineState.SAVED)) {
                 var message = _("Changes require restart of '%s'.").printf (machine.name);
                 window.notificationbar.display_for_action (message, _("_Restart"), () => {
                     machine.restart ();

src/vm-creator.vala
@@ -121,7 +121,7 @@ private class Boxes.VMCreator {
     private void on_machine_state_changed (GLib.Object object, GLib.ParamSpec? pspec = null) {
         var machine = object as LibvirtMachine;
 
-        if (machine.is_on ())
+        if (machine.is_on)
             return;
 
         if (machine.deleted) {

commit daf99fc6619ea5091222b81eb3bd6dd9b03bbe1c

libvirt-machine: Fix a condition
This is a regression from commit 03316ae, which resulted in us never
showing the IP address in properties for local libvirt machines.

src/libvirt-machine.vala
@@ -668,7 +668,7 @@ private class Boxes.LibvirtMachine: Boxes.Machine {
     }
 
     public string? get_ip_address () {
-        if (system_virt_connection == null || is_on)
+        if (system_virt_connection == null || !is_on)
             return null;
 
         var mac = get_mac_address ();