0c4402f8159ea1ff0a1a963696bf01d26c7c56b7
Emmanuele Bassi <ebassi@gnome.org>
2016-06-03 15:20:10 +0100

    build: Support non-srcdir builds
    The autogen.sh script should call the ancillary tools that modify the
    build from within the source directory, and call the configure script
    from the build directory.

--- autogen.sh
+++ autogen.sh
@@ -5,10 +5,14 @@ set -e # exit on errors
 srcdir=`dirname $0`
 test -z "$srcdir" && srcdir=.
 
+olddir=`pwd`
+
+cd "$srcdir"
 git submodule update --init --recursive
 autoreconf -v --force --install
 intltoolize -f
 
+cd "$olddir"
 if [ -z "$NOCONFIGURE" ]; then
     "$srcdir"/configure --enable-maintainer-mode --enable-vala --enable-debug ${1+"$@"}
 fi
