GNOME/gnome-boxes

commit ae21e562515ab60d3279f182313e9a6c7be53abd

unattended-setup-box: Cache setup data
Cache express install toggle, username, password and product key for
every media. No need to keep product keys in a separate file and having
to paste them each time you create a windows VM anymore. \o/

https://bugzilla.gnome.org/show_bug.cgi?id=693391

src/unattended-setup-box.vala
@@ -2,6 +2,12 @@
 
 [GtkTemplate (ui = "/org/gnome/Boxes/ui/unattended-setup-box.ui")]
 private class Boxes.UnattendedSetupBox : Gtk.Box {
+    private const string KEY_FILE = "setup-data.conf";
+    private const string EXPRESS_KEY = "express-install";
+    private const string USERNAME_KEY = "username";
+    private const string PASSWORD_KEY = "password";
+    private const string PRODUCTKEY_KEY = "product-key";
+
     public bool ready_for_express {
         get {
             return username != "" &&
@@ -90,15 +96,32 @@ private class Boxes.UnattendedSetupBox : Gtk.Box {
     private Gtk.Entry product_key_entry;
 
     private string? product_key_format;
+    private string media_path;
+    private GLib.KeyFile keyfile;
 
     public UnattendedSetupBox (InstallerMedia media, string? product_key_format, bool needs_internet) {
         this.product_key_format = product_key_format;
-        username_entry.text = Environment.get_user_name ();
 
-        setup_express_toggle (media.os_media.live, needs_internet);
         var msg = _("Express installation of %s requires an internet connection.").printf (media.label);
         needs_internet_label.label = msg;
         needs_internet_bar.visible = needs_internet;
+        media_path = media.device_file;
+        keyfile = new GLib.KeyFile ();
+
+        try {
+            var filename = get_user_unattended (KEY_FILE);
+            keyfile.load_from_file (filename, KeyFileFlags.KEEP_COMMENTS);
+
+            set_entry_text_from_key (username_entry, USERNAME_KEY, Environment.get_user_name ());
+            set_entry_text_from_key (password_entry, PASSWORD_KEY);
+            set_entry_text_from_key (password_entry, PASSWORD_KEY);
+            if (password != "")
+                password_notebook.next_page ();
+            set_entry_text_from_key (product_key_entry, PRODUCTKEY_KEY);
+        } catch (GLib.Error error) {
+            debug ("%s either doesn't already exist or we failed to load it: %s", media_path, error.message);
+        }
+        setup_express_toggle (media.os_media.live, needs_internet);
 
         if (product_key_format != null) {
             product_key_label.visible = true;
@@ -117,8 +140,27 @@ private class Boxes.UnattendedSetupBox : Gtk.Box {
         NetworkMonitor.get_default ().network_changed.disconnect (update_express_toggle);
     }
 
+    public void save_settings () {
+        keyfile.set_boolean (media_path, EXPRESS_KEY, express_install);
+        keyfile.set_string (media_path, USERNAME_KEY, username);
+        keyfile.set_string (media_path, PASSWORD_KEY, password);
+        keyfile.set_string (media_path, PRODUCTKEY_KEY, product_key);
+
+        var filename = get_user_unattended (KEY_FILE);
+        try {
+            keyfile.save_to_file (filename);
+        } catch (GLib.Error error) {
+            debug ("Error saving settings for '%s': %s", media_path, error.message);
+        }
+    }
+
     private void setup_express_toggle (bool live, bool needs_internet) {
-        express_toggle.active = !live;
+        try {
+            express_toggle.active = keyfile.get_boolean (media_path, EXPRESS_KEY);
+        } catch (GLib.Error error) {
+            debug ("Failed to read key '%s' under '%s': %s\n", EXPRESS_KEY, media_path, error.message);
+            express_toggle.active = !live;
+        }
 
         if (!needs_internet)
             return;
@@ -135,6 +177,20 @@ private class Boxes.UnattendedSetupBox : Gtk.Box {
             express_toggle.sensitive = false;
             express_toggle.active = false;
         }
+    }
+
+    private void set_entry_text_from_key (Gtk.Entry entry, string key, string? default_value = null) {
+        string? str = null;
+        try {
+            str = keyfile.get_string (media_path, key);
+        } catch (GLib.Error error) {
+            debug ("Failed to read key '%s' under '%s': %s\n", key, media_path, error.message);
+        }
+
+        if (str != null && str != "")
+            entry.text = str;
+        else if (default_value != null)
+            entry.text = default_value;
     }
 
     [GtkCallback]

src/unattended-installer.vala
@@ -143,6 +143,8 @@ private class Boxes.UnattendedInstaller: InstallerMedia {
     }
 
     public override async void prepare_for_installation (string vm_name, Cancellable? cancellable) {
+        setup_box.save_settings ();
+
         if (!setup_box.express_install) {
             debug ("Unattended installation disabled.");
 

commit ac552985647ebb6d7ee924cd77f0b93df44b4ff0

unattended-setup-box: Don't cache passwords in plain text
The user password specified during an express installation is cached
in plain text in ~/.config/gnome-boxes/unattended/setup-data.conf.
Instead, we should use the keyring - it's meant for this. As long as
the host user account has a password, the keyring will be encrypted
and protected in case the disk is stolen.

We store the password as a serialized GVariant dictionary because it
is more extensible than a simple string. In future we might want to
store multiple secrets for a particular media (eg., registration
passwords, product keys, etc.), and those can be accommodated by
specifying a different key in the dictionary.

Fallout from ae21e562515ab60d3279f182313e9a6c7be53abd

https://bugzilla.gnome.org/show_bug.cgi?id=777788

data/ui/unattended-setup-box.ui
@@ -189,6 +189,7 @@
                     <property name="text"></property>
                     <property name="visible">True</property>
                     <property name="visibility">False</property>
+                    <signal name="changed" handler="on_password_entry_changed"/>
                     <signal name="focus-out-event" handler="on_password_entry_focus_out"/>
                     <signal name="activate" handler="on_password_entry_activated"/>
                   </object>

src/unattended-setup-box.vala
@@ -97,7 +97,12 @@ private class Boxes.UnattendedSetupBox : Gtk.Box {
 
     private string? product_key_format;
     private string media_path;
+    private Cancellable cancellable = new Cancellable ();
     private GLib.KeyFile keyfile;
+    private Secret.Schema secret_password_schema
+            = new Secret.Schema ("org.gnome.Boxes",
+                                 Secret.SchemaFlags.NONE,
+                                 "gnome-boxes-media-path", Secret.SchemaAttributeType.STRING);
 
     public UnattendedSetupBox (InstallerMedia media, string? product_key_format, bool needs_internet) {
         this.product_key_format = product_key_format;
@@ -114,9 +119,45 @@ private class Boxes.UnattendedSetupBox : Gtk.Box {
 
             set_entry_text_from_key (username_entry, USERNAME_KEY, Environment.get_user_name ());
             set_entry_text_from_key (password_entry, PASSWORD_KEY);
-            if (password != "")
-                password_notebook.next_page ();
             set_entry_text_from_key (product_key_entry, PRODUCTKEY_KEY);
+
+            if (password != "") {
+                password_notebook.next_page ();
+            } else {
+                Secret.password_lookup.begin (secret_password_schema, cancellable, (obj, res) => {
+                    try {
+                        var credentials_str = Secret.password_lookup.end (res);
+                        if (credentials_str == null || credentials_str == "")
+                            return;
+
+                        try {
+                            var credentials_variant = GLib.Variant.parse (null, credentials_str, null, null);
+                            string password_str;
+                            if (!credentials_variant.lookup ("password", "s", out password_str))
+                                throw new Boxes.Error.INVALID ("couldn't unpack a string for the 'password' key");
+
+                            if (password_str != null && password_str != "") {
+                                password_entry.text = password_str;
+                                password_notebook.next_page ();
+                            }
+                        } catch (GLib.Error error) {
+                            warning ("Failed to parse password from the keyring: %s", error.message);
+                        }
+                    } catch (GLib.IOError.CANCELLED error) {
+                        return;
+                    } catch (GLib.Error error) {
+                        warning ("Failed to lookup password for '%s' from the keyring: %s",
+                                 media_path,
+                                 error.message);
+                    }
+                }, "gnome-boxes-media-path", media_path);
+            }
+
+            try {
+                keyfile.remove_key (media_path, PASSWORD_KEY);
+            } catch (GLib.Error error) {
+                debug ("Failed to remove key '%s' under '%s': %s", PASSWORD_KEY, media_path, error.message);
+            }
         } catch (GLib.Error error) {
             debug ("%s either doesn't already exist or we failed to load it: %s", KEY_FILE, error.message);
         }
@@ -135,6 +176,15 @@ private class Boxes.UnattendedSetupBox : Gtk.Box {
                 express_toggle.bind_property ("active", child, "sensitive", BindingFlags.SYNC_CREATE);
     }
 
+    public override void dispose () {
+        if (cancellable != null) {
+            cancellable.cancel ();
+            cancellable = null;
+        }
+
+        base.dispose ();
+    }
+
     public void clean_up () {
         NetworkMonitor.get_default ().network_changed.disconnect (update_express_toggle);
     }
@@ -142,7 +192,6 @@ private class Boxes.UnattendedSetupBox : Gtk.Box {
     public void save_settings () {
         keyfile.set_boolean (media_path, EXPRESS_KEY, express_install);
         keyfile.set_string (media_path, USERNAME_KEY, username);
-        keyfile.set_string (media_path, PASSWORD_KEY, password);
         keyfile.set_string (media_path, PRODUCTKEY_KEY, product_key);
 
         var filename = get_user_unattended (KEY_FILE);
@@ -150,6 +199,29 @@ private class Boxes.UnattendedSetupBox : Gtk.Box {
             keyfile.save_to_file (filename);
         } catch (GLib.Error error) {
             debug ("Error saving settings for '%s': %s", media_path, error.message);
+        }
+
+        if (password != null && password != "") {
+            var variant_builder = new GLib.VariantBuilder (GLib.VariantType.VARDICT);
+            var password_variant = new GLib.Variant ("s", password);
+            variant_builder.add ("{sv}", "password", password_variant);
+
+            var credentials_variant = variant_builder.end ();
+            var credentials_str = credentials_variant.print (true);
+
+            var label = _("GNOME Boxes credentials for '%s'").printf (media_path);
+            Secret.password_store.begin (secret_password_schema,
+                                         Secret.COLLECTION_DEFAULT,
+                                         label,
+                                         credentials_str,
+                                         null,
+                                         (obj, res) => {
+                try {
+                    Secret.password_store.end (res);
+                } catch (GLib.Error error) {
+                    warning ("Failed to store password for '%s' in the keyring: %s", media_path, error.message);
+                }
+            }, "gnome-boxes-media-path", media_path);
         }
     }
 
@@ -209,6 +281,12 @@ private class Boxes.UnattendedSetupBox : Gtk.Box {
     private void on_password_button_clicked () {
         password_notebook.next_page ();
         password_entry.grab_focus ();
+    }
+
+    [GtkCallback]
+    private void on_password_entry_changed () {
+        cancellable.cancel ();
+        cancellable = new Cancellable ();
     }
 
     [GtkCallback]