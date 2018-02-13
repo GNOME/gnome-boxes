GNOME/gnome-boxes

commit 30c2d638f8dc6605a6fc958ba1b605cdc03b2d0c

vm-configurator: Always update network config
Instead of only updating network config for existing domains if bridge
networking is available and existing networking in the box is not
already bridge network, let's always do so.

This fixes the issue of VM not starting if bridge networking is no
longer available (this has been known to happen due to setup/packaging
issues) and existing network is set to either bridge or some other
non-working configuration (e.g 'default' when importing from system
libvirt).

https://bugzilla.gnome.org/show_bug.cgi?id=756547

src/vm-configurator.vala
@@ -245,14 +245,11 @@ private class Boxes.VMConfigurator {
             warning ("Failed to update CPU config for '%s': %s", domain.name, e.message);
         }
 
-        if (!is_libvirt_bridge_net_available ())
-            return;
-
-        // First remove user and 'network' (used by system libvirt machines) interface device
+        // First remove existing network interface device
         GLib.List<GVirConfig.DomainDevice> devices = null;
         DomainInterface iface = null;
         foreach (var device in domain.get_devices ()) {
-            if (device is DomainInterfaceUser || device is DomainInterfaceNetwork)
+            if (device is DomainInterface)
                 iface = device as DomainInterface;
             else
                 devices.prepend (device);
@@ -262,8 +259,10 @@ private class Boxes.VMConfigurator {
 
         // If user interface device was found and removed, let's add bridge device now.
         if (iface != null) {
+            var bridge = is_libvirt_bridge_net_available ();
             var virtio = iface.get_model () == "virtio";
-            add_network_interface (domain, true, virtio);
+
+            add_network_interface (domain, bridge, virtio);
         }
     }
 

commit b1fcba2c217614519a6c14da8b324c76ea4d32a0

vm-configurator: Update network config only when necessary
Commit 30c2d638f has introduced a regression which causes the mac
address of a domain to change every time Boxes starts.

These changes create a new network interface only when the existing
one is not already bridged.

Fixes #158

src/vm-configurator.vala
@@ -257,7 +257,6 @@ private class Boxes.VMConfigurator {
             warning ("Failed to update CPU config for '%s': %s", domain.name, e.message);
         }
 
-        // First remove existing network interface device
         GLib.List<GVirConfig.DomainDevice> devices = null;
         DomainInterface iface = null;
         DomainGraphicsSpice graphics = null;
@@ -277,12 +276,15 @@ private class Boxes.VMConfigurator {
                 devices.prepend (device);
         }
 
-        // If user interface device was found and removed, let's add bridge device now.
+        // If user interface device was found and it needs to be bridged
         if (iface != null) {
             var bridge = is_libvirt_bridge_net_available ();
             var virtio = iface.get_model () == "virtio";
 
-            devices.prepend (create_network_interface (domain, bridge, virtio));
+            if (bridge)
+                devices.prepend (create_network_interface (domain, bridge, virtio));
+            else
+                devices.prepend (iface);
         }
 
         if (graphics != null)