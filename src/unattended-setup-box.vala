GNOME/gnome-boxes

commit 9746c634d4c4a6ec4fcdb1cf61112815d2347d68

unattended-installer/setup-box: Check libosinfo for optional passwords
libosinfo is able to tell us whether a given OS needs a password
for its install scripts. This way we can guarantee that users won't
perform Express Installations in which they will be locked out of
their session for not knowing the password created at installation.

Fixes #99

data/ui/unattended-setup-box.ui
@@ -192,6 +192,7 @@
                     <signal name="changed" handler="on_password_entry_changed"/>
                     <signal name="focus-out-event" handler="on_password_entry_focus_out"/>
                     <signal name="activate" handler="on_password_entry_activated"/>
+                    <signal name="notify::text" handler="on_mandatory_input_changed"/>
                   </object>
                 </child>
               </object>

src/unattended-setup-box.vala
@@ -11,6 +11,7 @@ private class Boxes.UnattendedSetupBox : Gtk.Box {
     public bool ready_for_express {
         get {
             return username != "" &&
+                   !needs_password &&
                    (product_key_format == null ||
                     product_key_entry.text_length == product_key_format.length);
         }
@@ -72,6 +73,25 @@ private class Boxes.UnattendedSetupBox : Gtk.Box {
 
     public signal void user_wants_to_create (); // User wants to already create the VM
 
+    private bool _needs_password;
+    private bool needs_password {
+        get {
+            if (password != "")
+                return false;
+
+            if (_needs_password) {
+                password_notebook.next_page ();
+                password_entry.grab_focus ();
+            }
+
+            return _needs_password;
+        }
+
+        set {
+            _needs_password = value;
+        }
+    }
+
     [GtkChild]
     private Gtk.InfoBar needs_internet_bar;
     [GtkChild]
@@ -110,6 +130,7 @@ private class Boxes.UnattendedSetupBox : Gtk.Box {
         var msg = _("Express installation of %s requires an internet connection.").printf (media.label);
         needs_internet_label.label = msg;
         needs_internet_bar.visible = needs_internet;
+        needs_password = (media as UnattendedInstaller).needs_password;
         media_path = media.device_file;
         keyfile = new GLib.KeyFile ();
 

src/unattended-installer.vala
@@ -27,6 +27,20 @@ private class Boxes.UnattendedInstaller: InstallerMedia {
         }
     }
 
+    public bool needs_password {
+        get {
+            foreach (var s in scripts.get_elements ()) {
+                var script = s as InstallScript;
+
+                var param = script.get_config_param (INSTALL_CONFIG_PROP_USER_PASSWORD);
+                if (param == null || param.is_optional ())
+                    return false;
+            }
+
+            return true;
+        }
+    }
+
     public UnattendedSetupBox setup_box;
 
     public File? disk_file;           // Used for installer scripts, user avatar and pre-installation drivers

commit 78123c25280596462fa87cd8a12a0bb3522d45be

unattended-setup-box: Don't steal input focus from username entry
When users are typing in the Username entry (in the Wizard), we
run checks that verify whether all the info required to perform an
express installation has already been inputed.

Commit 9746c63 introduced a bug in which the password entry was
grabing focus (and consequently stealing it from username entry)
for the period when the password entry is empty.

Fixes #347

data/ui/unattended-setup-box.ui
@@ -182,6 +182,9 @@
                     <property name="use-underline">True</property>
                     <property name="valign">baseline</property>
                     <signal name="clicked" handler="on_password_button_clicked"/>
+                    <style>
+                      <class name="suggested-action"/>
+                    </style>
                   </object>
                 </child>
                 <child>

src/unattended-setup-box.vala
@@ -79,11 +79,6 @@ private class Boxes.UnattendedSetupBox : Gtk.Box {
             if (password != "")
                 return false;
 
-            if (_needs_password) {
-                password_notebook.next_page ();
-                password_entry.grab_focus ();
-            }
-
             return _needs_password;
         }
 