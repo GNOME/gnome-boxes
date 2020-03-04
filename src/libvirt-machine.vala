GNOME/gnome-boxes

commit 2e2ba88edb3c1d2588a4a1d44ab7e3c6eeb56a99

vm-importer: Don't copy the VM storage automatically
Users want to connect to VMs wherever they are, instead of working
in local copies of them. For those that want the old behavior, we
will offer an option to "Import" the VM that will actually copy the
storage backend.

src/vm-importer.vala
@@ -20,18 +20,27 @@ private class Boxes.VMImporter : Boxes.VMCreator {
         machine.vm_creator = this;
         machine.config.access_last_time = (access_last_time > 0)? access_last_time : get_real_time ();
 
-        import_vm.begin (machine);
+        post_import_setup.begin (machine);
     }
 
     protected override async void continue_installation (LibvirtMachine machine) {
         install_media = yield MediaManager.get_instance ().create_installer_media_from_config (machine.domain_config);
         machine.vm_creator = this;
 
-        yield import_vm (machine);
+        yield post_import_setup (machine);
     }
 
     protected virtual async void post_import_setup (LibvirtMachine machine) {
         set_post_install_config (machine);
+
+        if (start_after_import) {
+            try {
+                machine.domain.start (0);
+            } catch (GLib.Error error) {
+                warning ("Failed to start domain '%s': %s", machine.domain.get_name (), error.message);
+            }
+        }
+        machine.vm_creator = null;
     }
 
     private async void import_vm (LibvirtMachine machine) {
@@ -53,15 +62,5 @@ private class Boxes.VMImporter : Boxes.VMCreator {
 
             return;
         }
-
-        yield post_import_setup (machine);
-        if (start_after_import) {
-            try {
-                machine.domain.start (0);
-            } catch (GLib.Error error) {
-                warning ("Failed to start domain '%s': %s", machine.domain.get_name (), error.message);
-            }
-        }
-        machine.vm_creator = null;
     }
 }

src/vm-configurator.vala
@@ -79,6 +79,9 @@ private class Boxes.VMConfigurator {
         clock.add_timer (timer);
         domain.set_clock (clock);
 
+        if (install_media is InstalledMedia)
+            target_path = install_media.device_file;
+
         set_target_media_config (domain, target_path, install_media);
         install_media.setup_domain_config (domain);
 

commit 22d2904142ee0650973fa30e7390f49d4c47a279

vm-importer, creator: Copy VMs disk while cloning
This is a regression introduced by 2e2ba88ed.

src/libvirt-machine.vala
@@ -749,7 +749,7 @@ private class Boxes.LibvirtMachine: Boxes.Machine {
 
             var vm_cloner = media.get_vm_creator ();
             var clone_machine = yield vm_cloner.create_vm (null);
-            vm_cloner.launch_vm (clone_machine, this.config.access_last_time);
+            vm_cloner.launch_vm (clone_machine, this.config.access_last_time, true);
 
             ulong under_construct_id = 0;
             under_construct_id = clone_machine.notify["under-construction"].connect (() => {

src/vm-creator.vala
@@ -63,7 +63,7 @@ private class Boxes.VMCreator : Object {
         return machine;
     }
 
-    public virtual void launch_vm (LibvirtMachine machine, int64 access_last_time = -1) throws GLib.Error {
+    public virtual void launch_vm (LibvirtMachine machine, int64 access_last_time = -1, bool clone = false) throws GLib.Error {
         if (!express_install) {
             ulong signal_id = 0;
 

src/vm-importer.vala
@@ -16,11 +16,15 @@ private class Boxes.VMImporter : Boxes.VMCreator {
         base.for_install_completion (machine);
     }
 
-    public override void launch_vm (LibvirtMachine machine, int64 access_last_time = -1) throws GLib.Error {
+    public override void launch_vm (LibvirtMachine machine, int64 access_last_time = -1, bool clone = false) throws GLib.Error {
         machine.vm_creator = this;
         machine.config.access_last_time = (access_last_time > 0)? access_last_time : get_real_time ();
 
-        post_import_setup.begin (machine);
+        if (clone) {
+            import_vm (machine);
+        } else {
+            post_import_setup.begin (machine);
+        }
     }
 
     protected override async void continue_installation (LibvirtMachine machine) {
@@ -62,5 +66,7 @@ private class Boxes.VMImporter : Boxes.VMCreator {
 
             return;
         }
+
+        yield post_import_setup (machine);
     }
 }