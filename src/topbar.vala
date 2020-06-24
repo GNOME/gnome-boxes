GNOME/gnome-boxes

commit 53d9e0609494875d05c32c8cafa6dc3510bdd0fd

Drop the filter switcher
See https://gitlab.gnome.org/GNOME/gnome-boxes/-/merge_requests/317#note_820522

data/ui/collection-toolbar.ui
@@ -8,12 +8,6 @@
       <class name="titlebar"/>
     </style>
 
-    <child type="title">
-      <object class="BoxesCollectionFilterSwitcher" id="filter_switcher">
-        <property name="visible">True</property>
-      </object>
-    </child>
-
     <child>
       <object class="GtkMenuButton" id="new_btn">
         <property name="visible">True</property>

data/gnome-boxes.gresource.xml
@@ -11,7 +11,6 @@
     <file>icons/eye-open-negative-filled-symbolic.svg</file>
     <file preprocess="xml-stripblanks">ui/app-window.ui</file>
     <file preprocess="xml-stripblanks">ui/auth-notification.ui</file>
-    <file preprocess="xml-stripblanks">ui/collection-filter-switcher.ui</file>
     <file preprocess="xml-stripblanks">ui/collection-toolbar.ui</file>
     <file preprocess="xml-stripblanks">ui/display-page.ui</file>
     <file preprocess="xml-stripblanks">ui/display-toolbar.ui</file>

data/ui/collection-filter-switcher.ui
@@ -1,54 +1,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<interface>
-  <template class="BoxesCollectionFilterSwitcher" parent="GtkButtonBox">
-    <property name="visible">True</property>
-    <property name="homogeneous">True</property>
-    <style>
-      <class name="linked"/>
-    </style>
-
-    <child>
-      <object class="GtkRadioButton" id="all_button">
-        <property name="visible">True</property>
-        <property name="draw-indicator">False</property>
-        <property name="label" translatable="yes" comments="Translators: this is a switch to show all boxes in main view.">All</property>
-        <signal name="toggled" handler="activate_button"/>
-      </object>
-
-      <packing>
-        <property name="expand">True</property>
-        <property name="fill">True</property>
-      </packing>
-    </child>
-
-    <child>
-      <object class="GtkRadioButton" id="local_button">
-        <property name="visible">True</property>
-        <property name="group">all_button</property>
-        <property name="draw-indicator">False</property>
-        <property name="label" translatable="yes" comments="Translators: this is a switch to show only local boxes in main view.">Local</property>
-        <signal name="toggled" handler="activate_button"/>
-      </object>
-
-      <packing>
-        <property name="expand">True</property>
-        <property name="fill">True</property>
-      </packing>
-    </child>
-
-    <child>
-      <object class="GtkRadioButton" id="remote_button">
-        <property name="visible">True</property>
-        <property name="group">all_button</property>
-        <property name="draw-indicator">False</property>
-        <property name="label" translatable="yes" comments="Translators: this is a switch to show only remote boxes in main view.">Remote</property>
-        <signal name="toggled" handler="activate_button"/>
-      </object>
-
-      <packing>
-        <property name="expand">True</property>
-        <property name="fill">True</property>
-      </packing>
-    </child>
-  </template>
-</interface>

src/collection-filter-switcher.vala
@@ -1,78 +1,0 @@
-// This file is part of GNOME Boxes. License: LGPLv2+
-
-[GtkTemplate (ui = "/org/gnome/Boxes/ui/collection-filter-switcher.ui")]
-private class Boxes.CollectionFilterSwitcher: Gtk.ButtonBox {
-    [GtkChild]
-    private Gtk.ToggleButton all_button;
-    [GtkChild]
-    private Gtk.ToggleButton local_button;
-    [GtkChild]
-    private Gtk.ToggleButton remote_button;
-
-    private Gtk.ToggleButton active_button;
-    private weak AppWindow window;
-
-    public void setup_ui (AppWindow window) {
-        this.window = window;
-        assert (window != null);
-
-        all_button.active = true;
-        activate_button (all_button);
-        App.app.call_when_ready (on_app_ready);
-
-        window.foreach_view ((view) => { view.filter.filter_func = null; });
-    }
-
-    private unowned CollectionFilterFunc? get_filter_func () {
-        if (active_button == all_button)
-            return null;
-        if (active_button == local_button)
-            return local_filter_func;
-        if (active_button == remote_button)
-            return remote_filter_func;
-        else
-            return null;
-    }
-
-    private bool local_filter_func (Boxes.CollectionItem item) {
-        assert (item != null && item is Machine);
-        var machine = item as Machine;
-
-        return machine.is_local;
-    }
-
-    private bool remote_filter_func (Boxes.CollectionItem item) {
-        assert (item != null && item is Machine);
-        var machine = item as Machine;
-
-        return !machine.is_local;
-    }
-
-    private void on_app_ready () {
-        update_sensitivity ();
-
-        App.app.collection.item_added.connect (update_sensitivity);
-        App.app.collection.item_removed.connect (update_sensitivity);
-    }
-
-    private void update_sensitivity () {
-        sensitive = (App.app.collection.length != 0);
-    }
-
-    [GtkCallback]
-    private void activate_button (Gtk.ToggleButton button) {
-        if (button == active_button)
-            return;
-
-        if (button.active)
-            active_button = button;
-
-        foreach (var child in get_children ()) {
-            var toggle_button = child as Gtk.ToggleButton;
-            if (toggle_button != null)
-                toggle_button.active = toggle_button == active_button;
-        }
-
-        window.foreach_view ((view) => { view.filter.filter_func = get_filter_func (); });
-    }
-}

src/collection-toolbar.vala
@@ -19,8 +19,6 @@ private class Boxes.CollectionToolbar: HeaderBar {
     private MenuButton downloads_hub_btn;
     [GtkChild]
     public MenuButton hamburger_btn;
-    [GtkChild]
-    private CollectionFilterSwitcher filter_switcher;
 
     private AppWindow window;
 
@@ -42,8 +40,6 @@ private class Boxes.CollectionToolbar: HeaderBar {
 
         window.notify["ui-state"].connect (ui_state_changed);
         App.app.notify["main-window"].connect (ui_state_changed);
-
-        filter_switcher.setup_ui (window);
 
         var builder = new Builder.from_resource ("/org/gnome/Boxes/ui/menus.ui");
         MenuModel menu = (MenuModel) builder.get_object ("app-menu");
@@ -114,7 +110,6 @@ private class Boxes.CollectionToolbar: HeaderBar {
             new_btn.show ();
             grid_btn.visible = window.view_type != AppWindow.ViewType.ICON;
             list_btn.visible = window.view_type != AppWindow.ViewType.LIST;
-            custom_title = filter_switcher;
             break;
 
         case UIState.CREDS:
@@ -124,7 +119,6 @@ private class Boxes.CollectionToolbar: HeaderBar {
             search_btn.hide ();
             grid_btn.hide ();
             list_btn.hide ();
-            custom_title = null;
             break;
 
         default:

src/meson.build
@@ -50,7 +50,6 @@ vala_sources = [
   'archive-writer.vala',
   'auth-notification.vala',
   'collection.vala',
-  'collection-filter-switcher.vala',
   'collection-toolbar.vala',
   'display-page.vala',
   'display-toolbar.vala',

commit 5600433b9fbd787000373e3cc84f39e228816105

topbar, collection-toolbar: Set CollectionToolbar title string
This is a regression introduced by commit 53d9e06.

When the user returns from the Display view to the collection view,
the HeaderBar title was set to an empty string. The behavior we
want, after removing the StackSwitcher, is to simply show the app
name.

data/ui/collection-toolbar.ui
@@ -4,6 +4,7 @@
   <template class="BoxesCollectionToolbar" parent="GtkHeaderBar">
     <property name="visible">True</property>
     <property name="show-close-button">True</property>
+    <property name="title" translatable="yes">Boxes</property>
     <style>
       <class name="titlebar"/>
     </style>

src/topbar.vala
@@ -54,7 +54,6 @@ private class Boxes.Topbar: Gtk.Stack, Boxes.UI {
         get { return _status; }
         set {
             _status = value;
-            collection_toolbar.set_title (_status);
             display_toolbar.set_title (_status);
         }
     }