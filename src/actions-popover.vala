GNOME/gnome-boxes

commit 821398577df54e01c51cd1246eae9887ed0d7599

collection, views: Remove "Favorite" option
Similarly to the removal of selection-mode, this decision is based
on the assumption that most of our users don't have a significant
amount of Machines to the point where selection-mode, categories,
favourites, are not really used/needed.

Users with more virtual machines than usual can still rely on the
search to find their VMs.

src/actions-popover.vala
@@ -3,7 +3,6 @@
 private class Boxes.ActionsPopover: Gtk.Popover {
     private const GLib.ActionEntry[] action_entries = {
         {"open-in-new-win", open_in_new_win_activated},
-        {"favorite",        favorite_activated},
         {"take_screenshot", take_screenshot_activated},
         {"force_shutdown",  force_shutdown_activated},
         {"delete",          delete_activated},
@@ -58,13 +57,6 @@ private class Boxes.ActionsPopover: Gtk.Popover {
             section.append (_("Take Screenshot"), "box.take_screenshot");
         }
 
-        // Favorite
-        if (("favorite" in machine.config.categories))
-            section.append (_("Remove from Favorites"), "box.favorite");
-        else
-            section.append (_("Add to Favorites"), "box.favorite");
-        menu.append_section (null, section);
-
         // New section for force shutdown and delete
         section = new GLib.Menu ();
 
@@ -109,12 +101,6 @@ private class Boxes.ActionsPopover: Gtk.Popover {
 
     private void open_in_new_win_activated () {
         App.app.open_in_new_window (window.current_item as Machine);
-    }
-
-    private void favorite_activated () {
-        var machine = window.current_item as Machine;
-        var enabled = !("favorite" in machine.config.categories);
-        machine.config.set_category ("favorite", enabled);
     }
 
     private string get_screenshot_filename () {

data/gtk-style.css
@@ -28,14 +28,6 @@
     border: none;
 }
 
-.favorite {
-  color: @theme_text_color;
-}
-
-.favorite.running {
-  color: white;
-}
-
 .boxes-product-key-entry {
     font-family: monospace;
 }

data/ui/icon-view-child.ui
@@ -16,16 +16,6 @@
             <property name="margin">10</property>
             <property name="margin-start">20</property>
             <property name="margin-end">20</property>
-
-            <child>
-              <object class="GtkImage" id="favorite">
-                <property name="visible">False</property>
-                <property name="icon-name">starred-symbolic</property>
-                <style>
-                  <class name="favorite"/>
-                </style>
-              </object>
-            </child>
           </object>
         </child>
 

data/ui/list-view-row.ui
@@ -59,17 +59,6 @@
     </child>
 
     <child>
-      <object class="GtkImage" id="favorite">
-        <property name="visible">True</property>
-        <property name="icon-name">starred-symbolic</property>
-      </object>
-      <packing>
-        <property name="expand">False</property>
-        <property name="fill">True</property>
-      </packing>
-    </child>
-
-    <child>
       <object class="GtkBox" id="name_and_info_box">
         <property name="visible">True</property>
         <property name="orientation">vertical</property>

src/box-config.vala
@@ -70,35 +70,6 @@ public class Boxes.BoxConfig: GLib.Object, Boxes.IConfig {
         save ();
     }
 
-    private void remove_category (string category) {
-        string[] categories = {};
-
-        foreach (var it in this.categories)
-            if (it != category)
-                categories += it;
-
-        this.categories = categories;
-    }
-
-    private void add_category (string category) {
-        if (category in categories)
-            return;
-
-        // FIXME: vala bug if in one line
-        string[] categories = categories;
-        categories += category;
-        this.categories = categories;
-    }
-
-    public void set_category (string category, bool enabled) {
-        if (enabled)
-            add_category (category);
-        else
-            remove_category (category);
-
-        save ();
-    }
-
     private void save_property (Object object, string property_name) {
         var value = Value (object.get_class ().find_property (property_name).value_type);
 
@@ -186,15 +157,7 @@ public class Boxes.BoxConfig: GLib.Object, Boxes.IConfig {
     }
 
     public int compare (BoxConfig other) {
-        // sort favorites first
-        // FIXME: We should probably have some fixed categories rather than hard-coded strings
-        if ("favorite" in categories) {
-            if (!("favorite" in other.categories))
-                return -1;
-        } else if ("favorite" in other.categories)
-            return 1;
-
-        // then by last time used
+        // sort by last time used
         if (access_last_time > 0 || other.access_last_time > 0) {
             if (access_last_time > other.access_last_time)
                 return -1;

src/collection.vala
@@ -140,21 +140,3 @@ private class Boxes.CollectionFilter: GLib.Object {
 }
 
 private delegate bool Boxes.CollectionFilterFunc (Boxes.CollectionItem item);
-
-private class Boxes.Category: GLib.Object {
-    public enum Kind {
-        USER,
-        NEW,
-        FAVORITES,
-        PRIVATE,
-        SHARED
-    }
-
-    public string name;
-    public Kind kind;
-
-    public Category (string name, Kind kind = Kind.USER) {
-        this.name = name;
-        this.kind = kind;
-    }
-}

src/icon-view-child.vala
@@ -21,8 +21,6 @@ private class Boxes.IconViewChild : Gtk.Box {
     [GtkChild]
     private unowned Gtk.Spinner spinner;
     [GtkChild]
-    private unowned Gtk.Image favorite;
-    [GtkChild]
     private unowned Gtk.Image running_thumbnail;
     [GtkChild]
     private unowned Gtk.Label machine_name;
@@ -39,16 +37,13 @@ private class Boxes.IconViewChild : Gtk.Box {
         stack.width_request = SCREENSHOT_WIDTH;
         stack.height_request = SCREENSHOT_HEIGHT;
 
-        machine.config.notify["categories"].connect (update_favorite);
         machine.notify["under-construction"].connect (update_thumbnail);
         machine.notify["is-stopped"].connect (update_thumbnail);
         thumbnailer.notify["thumbnail"].connect (() => {
             update_thumbnail ();
-            update_favorite ();
         });
 
         update_thumbnail ();
-        update_favorite ();
 
         machine.bind_property ("name", machine_name, "label", BindingFlags.SYNC_CREATE);
     }
@@ -77,14 +72,5 @@ private class Boxes.IconViewChild : Gtk.Box {
         }
 
         spinner.stop ();
-    }
-
-    private void update_favorite () {
-        favorite.visible = "favorite" in machine.config.categories;
-
-        if (thumbnailer.thumbnail != null)
-            favorite.get_style_context ().add_class ("running");
-        else
-            favorite.get_style_context ().remove_class ("running");
     }
 }

src/icon-view.vala
@@ -10,18 +10,7 @@ private class Boxes.IconView: Gtk.ScrolledWindow {
     private AppWindow window;
     private Boxes.ActionsPopover context_popover;
 
-    private Category _category;
-    public Category category {
-        get { return _category; }
-        set {
-            _category = value;
-            // FIXME: update view
-        }
-    }
-
     construct {
-        category = new Category (_("New and Recent"), Category.Kind.NEW);
-
         filter = new CollectionFilter ();
         filter.notify["text"].connect (() => {
             flowbox.invalidate_filter ();

commit 837d4cdd124db8caab2886c05fc72d459994203a

actions-popover: Re-add display actions to menu
This is the fix for a regression introduced by commit
821398577df54e01c51cd1246eae9887ed0d7599

Fixes #753

src/actions-popover.vala
@@ -56,6 +56,7 @@ private class Boxes.ActionsPopover: Gtk.Popover {
             // Take Screenshot
             section.append (_("Take Screenshot"), "box.take_screenshot");
         }
+        menu.append_section (null, section);
 
         // New section for force shutdown and delete
         section = new GLib.Menu ();